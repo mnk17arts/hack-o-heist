3fdda83cd301bd3119d790ec075d635691d646ac
Pawan Singh <pmarsingh@gmail.com>
2021-09-30 22:56:51 +0530

    Prizes

--- src/Components/Prizes/Prizes.js
+++ src/Components/Prizes/Prizes.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Prizes.css";
-import prizes from "./prizes.svg";
+import prize1 from "./prize1.svg";
+import prize2 from "./prize2.svg";
+import prize3 from "./prize3.svg";
+import prize4 from "./prize4.svg";
 
 const Prizes = () => {
   return (
@@ -9,13 +12,16 @@ const Prizes = () => {
       <h1 className="h_name">Prizes</h1>
       <div className="PrizesContainer">
         <div className="cont1">
-          <img src={prizes} alt="" />
+          <img src={prize2} alt="" />
         </div>
         <div className="cont1">
-          <img src={prizes} alt="" />
+          <img src={prize1} alt="" />
         </div>
         <div className="cont1">
-          <img src={prizes} alt="" />
+          <img src={prize3} alt="" />
+        </div>
+        <div className="cont1">
+          <img src={prize4} alt="" />
         </div>
       </div>
     </div>

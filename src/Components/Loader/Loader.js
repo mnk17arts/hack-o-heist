bf4391c4c0f1a4633a31c92ed166e2038c9c28e9
Pawan Singh <pmarsingh@gmail.com>
2021-09-30 23:34:16 +0530

    About us

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import './Loader.css'
+import React from "react";
+import "./Loader.css";
 import logo from "../Register/img/logo4.png";
 
 const Loader = () => {
-    return (
-        <div id="caseBlanche">
-            <div>
-                <div id="rond">
-                    <div id="test"></div>
-                </div>
-                <div id="load">
-                    <img src={logo} alt=""/>
-                </div>
-            </div>
+  return (
+    <div id="caseBlanche">
+      <div className="caseLoad">
+        <div id="rond">
+          <div id="test"></div>
         </div>
-    )
-}
+        <div className="img-load">
+          <img src={logo} alt="" width="250" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
